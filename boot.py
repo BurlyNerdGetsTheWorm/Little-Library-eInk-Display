2a2e3a2f3d01adf9ba87764eac0d570ad3504fe5
BurlyNerdGetsTheWorm <gdotmarsh@gmail.com>
2021-05-07 01:38:34 -0400

    Reversed boot file to prioritize read-only

--- boot.py
+++ boot.py
@@ -12,4 +12,7 @@ switch.pull = digitalio.Pull.UP
 
 # If the the B button on the eink is pressed during startup, CircuitPython can write to the drive.
 # Otherwise, if not pressed, the computer can edit the files
-storage.remount("/", switch.value)
+# storage.remount("/", switch.value)
+# The below code should give the board priority *unless* the button is pressed during startup.
+# AKA press the B button on eink when you need to edit/save code
+storage.remount("/", not switch.value)
